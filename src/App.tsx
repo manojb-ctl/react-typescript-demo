58d85dc9d202231ad4e5efed7df0c39a986baf9d
manoj <manojb@crystaltechnolab.com>
2022-07-04 17:36:27 +0530

    react-typescript demo done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import "./App.css";
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  return <div></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHanlder = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHanlder = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  return (
+    <div>
+      <NewTodo onAddTodo={addTodoHanlder} />
+      <Todos items={todos} onRemoveTodo={removeTodoHanlder} />
+    </div>
+  );
 }
 
 export default App;
+
+// For TypeScript with Context API :-
+// https://github.com/academind/react-complete-guide-code/tree/27-react-typescript/code/16-finished
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+import classes from "./NewTodo.module.css";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHanler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHanler} className={classes.form}>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
